91b206d2f190aa22f91e9579d1db13a56ca3e8fe
Maksym Kazhaiev <mkazhaev746@gmail.com>
2023-01-12 12:17:30 +0100

    integrated html and css into flask

--- feeder/__init__.py
+++ feeder/__init__.py
@@ -1,32 +1,44 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder='static')
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'feeder.sqlite'),
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def main():
+        return render_template('index.html')
 
+    @app.route('/flightradar-configuration')
+    def flightradar_configuration():
+        return render_template('/pages/flightradar24/configuration.html')
+
+    @app.route('/flightaware-configuration')
+    def flightaware_configuration():
+        return render_template('/pages/flightaware/configuration.html')
+    
+    @app.route('/opensky-configuration')
+    def opensky_configuration():
+        return render_template('/pages/opensky/configuration.html')
+    
     return app
